36b557b86aef78cc42df216ce2bb6bd2cd47f8a9
Stan Hutcheon <stanhutcheon@bigfork.co.uk>
2015-01-19 13:41:25 +0000

    Add option to fetch tags from global var

--- javascript/TaggedField.js
+++ javascript/TaggedField.js
@@ -3,7 +3,12 @@
 
 	$('input.jquery-tagit-hook').entwine({
 		onmatch: function() {
+			var tags;
+			if(window.TaggedFieldList) {
+				tags = window.TaggedFieldList;
+			}
 			$('input.jquery-tagit-hook').tagit({
+				availableTags: tags,
 				beforeTagAdded: function(event, ui) {
 					ui.tag.addClass('search-choice');
 				}
